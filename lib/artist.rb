2afeb2f556eb2bfc6fa9ef44bc0f0b9cca2f9348
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-03 02:11:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,47 +1,61 @@
 require 'pry'
-class Artist 
 
-attr_accessor :name, :song
-extend Concerns::Findable
+class Artist
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
+  @@all = []
 
-@@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def initialize(name)
-  @name = name 
-  @@all << self
-  @songs = []
-  
-end 
+  def self.all
+    @@all
+  end
 
-def self.all 
-  @@all 
-end
+  def self.destroy_all
+    @@all.clear
+  end
 
-def self.destroy_all
-  @@all.clear
-end
+  def save
+    @@all << self
+  end
 
-def save
-  @@all << self
-end
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
+  end
 
-def self.create(name) 
-  artist = self.new(name)
-  artist.save
-  artist
-end 
+  def songs
+    @songs
+  end
 
-def songs 
- Song.all
-end
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+    if @songs.include?(song)
+      nil
+    else
+      @songs << song
+    end
+    song
+  end
 
-def add_song(song)
-   song.artist = self unless song.artist
-   songs << song unless songs.include?(song)
-end
-
-def genres 
-  songs.collect {|song| song.genre}.uniq
-end
+  def genres
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.genre)
+        nil
+      else
+        @new_array << song.genre
+      end
+    end
+    @new_array
+  end
 
 end
