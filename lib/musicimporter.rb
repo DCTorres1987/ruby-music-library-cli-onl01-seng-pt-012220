72b5b6133e5c695f81fa4ad4fa3ff3901eb327a6
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-03 02:16:23 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -6,7 +6,14 @@ class MusicImporter
     @path = path
   end
   
-  def files 
+  def files
+    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
   end
   
 end
