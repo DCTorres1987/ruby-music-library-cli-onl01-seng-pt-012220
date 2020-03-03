62c308cf409ba53b0efc2b5acadbc864075af42c
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-03 02:11:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,47 +1,49 @@
-class Genre 
-  
-  attr_accessor :name 
+require 'pry'
+
+class Genre
+  attr_accessor :name, :musiclibrarycontroller, :musicimporter
   extend Concerns::Findable
-  
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
-    @@all << self
+    @name = name
     @songs = []
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
+  end
+
+  def self.all
+    @@all
+  end
+
   def self.destroy_all
-    @@all.clear 
+    @@all.clear
   end
-  
-  def save 
-    @@all << @name 
+
+  def save
+    @@all << self
   end
-  
-  def self.create(name)
-    genre = self.new(name)
+
+  def self.create(genre)
+    genre = self.new(genre)
     genre.save
     genre
   end
-  
-  def songs 
-    @songs 
+
+  def songs
+    @songs
   end
-  
-  def add_song(song)
-   song.genre = self unless song.genre
-   songs << song unless songs.include?(song)
+
+  def artists
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
   end
-  
-  def artists 
-     # self.songs.collect {|song| song.genre}
-    self.songs.collect {|s| s.artist}.uniq
-  end  
-  
+
+
 end
   
