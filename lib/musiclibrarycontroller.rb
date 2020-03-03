6dca04a468e84de5dd6d75a7c0bf7f56e88a7581
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-03 02:19:14 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,3 +1,12 @@
-class MusicLibraryController
+require 'pry'
 
+class Musi
+  
+  attr_accessor :path
+
+  def initialize(path='./db/mp3s')
+    @path = path
+    MusicImporter.new(path).import
+  end
+  
 end
